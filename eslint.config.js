66413b7671d96e3dd87ba8c2dcdd7027073eb6b7
Robin <lotosv2010@163.com>
2025-06-07 00:50:37 +0800

    feat(lerna): 配置eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+const { defineConfig, globalIgnores } = require("eslint/config");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+module.exports = defineConfig([
+  globalIgnores([
+    "node_modules",
+    "dist",
+    "coverage",
+    "**/__tests__/**",
+    "eslint.config.js",
+    "jest.config.js"
+  ]),
+	{ files: ["**/*.js"], languageOptions: { globals: { ...globals.node, ...globals.es2021, ...globals.browser }  } },
+	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+]);
