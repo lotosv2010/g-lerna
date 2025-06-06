0d4339de65c0867737f96d80d5350e58e5161e48
Robin <lotosv2010@163.com>
2025-06-07 01:39:38 +0800

    feat(lerna): 配置prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 const { defineConfig, globalIgnores } = require("eslint/config");
 const globals = require("globals");
 const js = require("@eslint/js");
+const prettier = require("eslint-plugin-prettier");
 
 module.exports = defineConfig([
   globalIgnores([
@@ -9,8 +10,9 @@ module.exports = defineConfig([
     "coverage",
     "**/__tests__/**",
     "eslint.config.js",
-    "jest.config.js"
+    "jest.config.js",
+    "prettier.config.js",
   ]),
 	{ files: ["**/*.js"], languageOptions: { globals: { ...globals.node, ...globals.es2021, ...globals.browser }  } },
-	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.js"], plugins: { js, prettier }, extends: ["js/recommended"], rules: { "prettier/prettier": "error" } },
 ]);
